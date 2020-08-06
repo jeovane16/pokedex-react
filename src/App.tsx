2824f1c1a147c0c74e494b58bbbf712a6e8a6987
jeovane16 <jeovanearaujo16@gmail.com>
2020-08-06 19:01:23 -0300

    Adicionado o componente MainConfirm

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { MainHome } from './components/MainHome/MainHome';
+import { MainConfirm } from "./components/MainConfirm/MainConfirm";
+import man from './assets/man.png'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MainHome/>
+      <MainConfirm name='Asperoon' trainer={man}/>
       <Footer/>
     </div>
   );
