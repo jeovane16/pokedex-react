94dcf6e6eac175c3cf250168411b91d038d6fdd8
jeovane16 <jeovanearaujo16@gmail.com>
2020-08-06 11:13:30 -0300

    Estrutura inicial do projeto e adicionado o componente Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header className='containerHeader'>
+      <h1>Pokédex</h1>
+    </header>
+  );
+};
